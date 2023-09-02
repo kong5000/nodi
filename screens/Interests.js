40f6ee49e3af9452afefd8900badb3aad1645a12
k <keith.ong5000@gmail.com>
2023-09-02 16:29:06 -0700

    updated icons for interests, styling changes

--- screens/Interests.js
+++ screens/Interests.js
@@ -6,20 +6,98 @@ import { COLORS, FLEX_CENTERED, TEXT_STYLES } from '../style'
 import NextButton from '../components/NextButton';
 import StyleText from '../components/StyleText'
 const { width, height } = Dimensions.get('window');
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const thingsToDo = [
-    { text: "Festivals", emoticon: "🎊" },
-    { text: "Road Trips", emoticon: "🚗" },
-    { text: "Beaches", emoticon: "🏖️" },
-    { text: "Markets", emoticon: "🧺" },
-    { text: "Nature", emoticon: "🌳" },
-    { text: "Architecture", emoticon: "🏛️" },
-    { text: "Museums", emoticon: "🖼️" },
-    { text: "Hiking", emoticon: "⛰️" },
-    { text: "Sports", emoticon: "🏟️" },
-    { text: "Concerts", emoticon: "🎤" },
-    { text: "Nightlife", emoticon: "🍸" },
-    { text: "Art", emoticon: "🎭" },
+    {
+        text: "Festivals", icon: <MaterialCommunityIcons
+            name="party-popper" size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Road Trips", icon: <MaterialIcons
+            name="drive-eta"
+            size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+
+    },
+    {
+        text: "Beaches", icon: <FontAwesome5
+            name="umbrella-beach"
+            size={20}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Markets", icon: <FontAwesome5
+            name="shopping-basket"
+            size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Nature", icon: <FontAwesome5
+            name="tree"
+            size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Museums", icon: <MaterialIcons
+            name="museum"
+            size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Hiking", icon: <FontAwesome5
+            name="mountain"
+            size={20}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Sports", icon: <FontAwesome5
+            name="basketball-ball"
+            size={24}
+            color={COLORS.mainTheme}
+            style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Concerts", icon: <FontAwesome5
+            name="music"
+            size={24}
+            color={COLORS.mainTheme} style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Nightlife", icon: <FontAwesome5
+            name="cocktail"
+            size={24}
+            color={COLORS.mainTheme} style={{ marginRight: '10%' }}
+        />
+    },
+    {
+        text: "Art", icon: <FontAwesome5
+            name="paint-brush"
+            size={24}
+            color={COLORS.mainTheme} style={{ marginRight: '10%' }}
+        />
+    },
 ]
 
 const foodAndDrink = [
@@ -54,6 +132,7 @@ const Interests = ({ setPage }) => {
             {thingsToDo.map((item) =>
                 <TouchableOpacity onPress={() => toggleInterest(item.text)}>
                     <View style={[styles.activity, interests.includes(item.text) ? styles.enabled : styles.activityDisabled]}>
+                        {item.icon}
                         <StyleText
                             text={item.text}
                             fontSize={16}
@@ -76,6 +155,8 @@ const styles = StyleSheet.create({
     },
     activity: {
         ...FLEX_CENTERED,
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
         paddingVertical: 15,
         paddingHorizontal: 15,
         borderWidth: 1,
