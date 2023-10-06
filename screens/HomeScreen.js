24f94ce129d3619fbb7d5dd401c9ac69f4303af8
k <keith.ong5000@gmail.com>
2023-10-06 10:04:02 -0700

    added notification test code, working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,116 @@
 import {
+    Button,
     StyleSheet, View,
 } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import { getCards } from '../services/Utils'
 import Footer from '../components/Footer'
 import ParallaxCarousel from '../components/ParallaxCarousel'
 import getUserData from '../hooks/userData'
 import * as Location from 'expo-location';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import StyleText from '../components/StyleText'
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+
 
 const HomeScreen = () => {
+    const [token, setToken] = useState('')
+    
+    // Can use this function below or use Expo's Push Notification Tool from: https://expo.dev/notifications
+    async function sendPushNotification(expoPushToken) {
+        const message = {
+            to: expoPushToken,
+            sound: 'default',
+            title: 'Original Title',
+            body: 'And here is the body!',
+            data: { someData: 'goes here' },
+        };
+
+        await fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        });
+    }
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+        // if (Device.isDevice) {
+        if (true) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = await Notifications.getExpoPushTokenAsync({
+                projectId: Constants.expoConfig.extra.eas.projectId,
+            });
+            setToken(JSON.stringify(token))
+            console.log(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+
+        if (Platform.OS === 'android') {
+            Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        return token;
+    }
+
     const { user } = useAuth()
     const { userData } = getUserData()
     // const [trips, setTrips] = useState([])
-
     const [items, setItems] = useState([])
 
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
 
     useEffect(() => {
         (async () => {
@@ -47,9 +140,38 @@ const HomeScreen = () => {
 
     return (
         <View style={styles.screen}>
-            <ParallaxCarousel
-                items={items}
+            <View style={{
+                backgroundColor: "green",
+                width: "100%",
+                padding: 10,
+            }}>
+                <StyleText
+                    text={notification ? JSON.stringify(notification) : "Notification Will be here"}
+                    fontSize={30}
+                />
+                <StyleText
+                    text={token ? token : "token Will be here"}
+                    fontSize={30}
+                />
+            </View>
+            <Button
+                style={{
+                    height: 100,
+                    width: 100,
+                    backgroundColor: 'red'
+                }}
+                title="Press to Send Notification"
+                onPress={async () => {
+                    try {
+                        await sendPushNotification(expoPushToken);
+                    } catch (err) {
+                        console.log(err)
+                    }
+                }}
             />
+            {/* <ParallaxCarousel
+                items={items}
+            /> */}
             <Footer />
         </View>
     )
